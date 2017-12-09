5d0d08d4ac06747e482c692eb4ccdbbe8cef25a7
Kent Michelsen <kentmichelsen@gmail.com>
2017-12-09 19:33:14 +0100

    Rettelser

---
+++ assets/form.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+
+    $('#contact_form').on('submit', function(e) {
+        e.preventDefault();
+        
+        //get the name field value
+        var name = $('#name').val();
+        //get the name field value
+        var email = $('#email').val();
+		//get thesubject
+		var subject = $('#subject').val();
+        //get the comments
+        var comments = $('#comments').val();
+                    
+        //pretend we don't need validation
+        
+        //send to formspree
+        $.ajax({
+            url:'https://formspree.io/kentmichelsen@gmail.com',
+            method:'POST',
+            data:{
+                name:name,
+                _replyto:email,
+                 email:email,
+                comments:comments,
+                subject:subject,
+            },
+            dataType:"json",
+            success:function() {
+                console.log('success'); 
+                //$('#formBlock').hide();
+                //$('#thankyouBlock').show();
+				top.location.href="tak.html";
+            }   
+
+        });     
+        
+    });
+
+}); 
